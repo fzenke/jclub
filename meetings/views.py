a21dac66d03d45be804f71c40ce7a79ed2c24240
Friedemann Zenke <friedemann.zenke@epfl.ch>
2014-09-11 19:50:04 +0200

    Adds /meetings index.html template

--- meetings/views.py
+++ meetings/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 
 from meetings.models import Meeting,TimeSlot
 
 # Create your views here.
 
 def index(request):
-    upcoming_meetings_list = Meeting.objects.order_by('timeslot__date_time')[:5]
-
-    output = ', '.join([p.publication_reference for p in upcoming_meetings_list])
-    return HttpResponse(output)
+    upcoming_meetings_list = Meeting.objects.order_by('timeslot__date_time')[:3]
+    template = loader.get_template('meetings/index.html')
+    context = RequestContext(request, {
+        'upcoming_meetings_list': upcoming_meetings_list,
+    })
+    return HttpResponse(template.render(context))
 
 def detail(request, meeting_id):
     return HttpResponse("You're looking at the TODO detail view of meeting %s." % meeting_id)
