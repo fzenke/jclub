e01de1c669392e0a149d9e5e11c4ad0b715c8993
Friedemann Zenke <friedemann.zenke@epfl.ch>
2014-09-12 12:41:40 +0200

    Experimental changes to index.html template and view.

--- meetings/views.py
+++ meetings/views.py
@@ -8,9 +8,15 @@ from meetings.models import Meeting,TimeSlot
 
 def index(request):
     upcoming_meetings_list = Meeting.objects.order_by('timeslot__date_time')[:3]
+    upcoming_timeslots_list = Meeting.objects.order_by('timeslot__date_time')[:3]
+    upcoming_presenters_list = Meeting.objects.order_by('timeslot__date_time')[:3]
+
+
     template = loader.get_template('meetings/index.html')
     context = RequestContext(request, {
         'upcoming_meetings_list': upcoming_meetings_list,
+        'upcoming_timeslots_list': upcoming_timeslots_list,
+        'upcoming_presenters_list': upcoming_presenters_list,
     })
     return HttpResponse(template.render(context))
 
