a2a79a1d5d5f5da93a9dac3520db6bce1df96228
Alex Seeholzer <seeholzer@gmail.com>
2014-09-12 14:29:04 +0200

    added view filtering for users, timeslots, meetings

--- jclub/apps/meetings/views.py
+++ jclub/apps/meetings/views.py
@@ -1,19 +1,35 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
-
 from jclub.apps.meetings.models import Meeting,TimeSlot
+from django.contrib.auth.models import User
+from datetime import *
+from collections import OrderedDict
 
 # Create your views here.
 
 def index(request):
-    upcoming_meetings_list = Meeting.objects.order_by('timeslot__date_time')[:3]
-    upcoming_timeslots_list = Meeting.objects.order_by('timeslot__date_time')[:3]
-    upcoming_presenters_list = Meeting.objects.order_by('timeslot__date_time')[:3]
 
+    # get next meetings from today and order by date
+    current_day = datetime.now()
+    current_day.replace(hour=0, minute=0, second=0, microsecond=0)
+    upcoming_meetings_list = Meeting.objects.filter(timeslot__date_time__gte=current_day).order_by('timeslot__date_time')[:3]
+    
+    # get untaken timeslots and order by date
+    upcoming_timeslots_list = TimeSlot.objects.filter(meeting__isnull=True).order_by('meeting__timeslot__date_time')[:3]
+    
+    # get 1) presenters that are not assigned to a slot 2) that presented the longest ago
+    upcoming_presenters_list_empty = User.objects.filter(meeting__isnull=True)[:5]
+    
+    # this works on postgresql but sadly not on anything else
+    
+    # upcoming_presenters_list_given = User.objects.filter(meeting__isnull=False).order_by("meeting__timeslot__date_time").distinct('id')[:3]
+    # TODO filter for distinct results here
+    upcoming_presenters_list_given = User.objects.filter(meeting__isnull=False).order_by("meeting__timeslot__date_time")[:5]
+    upcoming_presenters_list = list(upcoming_presenters_list_empty) + list(upcoming_presenters_list_given)
 
     template = loader.get_template('meetings/index.html')
-    print "aha", template
+    
     context = RequestContext(request, {
         'upcoming_meetings_list': upcoming_meetings_list,
         'upcoming_timeslots_list': upcoming_timeslots_list,
@@ -23,3 +39,4 @@ def index(request):
 
 def detail(request, meeting_id):
     return HttpResponse("You're looking at the TODO detail view of meeting %s." % meeting_id)
+
