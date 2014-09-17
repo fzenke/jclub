448973687b8994c82cb8772939ffd784bf4e19e4
Alex Seeholzer <seeholzer@gmail.com>
2014-09-17 11:18:41 +0200

    replace explicit urls by dynamic urls; reformatted index with page headers; add admin links;

--- jclub/apps/meetings/urls.py
+++ jclub/apps/meetings/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from jclub.apps.meetings import views
 
 urlpatterns = patterns('',
-    url(r'^meetings/(?P<meeting_id>\d+)/$', views.detail, name='detail'),
+    url(r'^meetings/(?P<meeting_id>\d+)/$', views.detail, name='meetings_detail'),
     url(r'^meetings/$', views.meetings_index, name='meetings_index'),
     url(r'^$', views.index, name='index'),
 )
