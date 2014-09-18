58b96235d42c8fc900052fcf7bece148858c7a04
Alex Seeholzer <seeholzer@gmail.com>
2014-09-18 17:19:41 +0200

    overwritten unicode representation of the user for admin back end

--- jclub/apps/meetings/models.py
+++ jclub/apps/meetings/models.py
@@ -30,3 +30,19 @@ class Meeting(models.Model):
     def __unicode__(self):
         return u'%s: %s %s' % (self.timeslot.date_time.strftime('%a, %d.%m.%Y @ %H:%M'), self.presenter.first_name, self.presenter.last_name)
 
+# These functions extend the contrib.auth.User without actually writing a linked user_profile
+# or subclassing the User class. This, for now, is the most straightforward way of
+# implementing the desired features.
+# 
+# TODO replace this with a proper subclass of the User class.
+def user_unicode(self):
+    c = []
+    if self.last_name:
+        c.append(self.last_name)
+    if self.first_name:
+        c.append(self.first_name)
+    if c:
+        return ', '.join(c)
+    else:
+        return self.user_name
+User.add_to_class('__unicode__', user_unicode)
