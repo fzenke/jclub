e05254a2264089966f3406960edaab16de7ad025
Alex Seeholzer <seeholzer@gmail.com>
2014-09-18 08:56:27 +0200

    meeting editing restrictions moved from forms.py to admin.py; now also user selection is limited for non-superusers;

--- jclub/apps/meetings/admin.py
+++ jclub/apps/meetings/admin.py
@@ -1,11 +1,36 @@
 from django.contrib import admin
 from jclub.apps.meetings.models import Meeting,TimeSlot,Category
 from jclub.apps.meetings.forms import MeetingAdminForm
+from django.db.models import Q
 
 # Custom admin backend form - this restricts the timeslots dropdown 
-# to only the ones not assigned to a meeting
+# to only the ones not assigned to a meeting, as well as the users for non-admin users
+# to only himself and the currently assigned one.
+
+# TODO check on save if user is allowed to save the chosen presenter. Currently
+# this just restricts the selection, probably a modified request could smuggle in
+# any user as presenter.
+
 class MeetingAdmin(admin.ModelAdmin):
-    form = MeetingAdminForm
+
+    def get_form(self, request, obj=None, **kwargs):
+
+        form = super(MeetingAdmin, self).get_form(request, obj, **kwargs)
+        
+        # restrict presenters to current + self for non-superusers 
+        if not request.user.is_superuser:
+            idset = []
+            if obj:
+                idset=[obj.presenter.id]
+            form.base_fields['presenter'].queryset = form.base_fields['presenter'].queryset.filter(Q(id=request.user.id) | Q(id__in=idset))
+
+        # restrict timeslots to current + available
+        idset = []
+        if obj:
+            idset=[obj.id]
+        form.base_fields['timeslot'].queryset = form.base_fields['timeslot'].queryset.filter(Q(meeting__isnull=True) | Q(meeting__id__in=idset))
+            
+        return form
 
 admin.site.register(Meeting, MeetingAdmin)
 admin.site.register(TimeSlot)
--- jclub/apps/meetings/forms.py
+++ jclub/apps/meetings/forms.py
@@ -2,21 +2,22 @@ from django import forms
 from django.db.models import Q
 from jclub.apps.meetings.models import Meeting, TimeSlot
 
+# The following functionality has been moved to admin.py for cleaner code:
 # This restricts the timeslots dropdown to only the ones not assigned to a meeting
+
 class MeetingAdminForm(forms.ModelForm):
 
-    timeslot = forms.ModelChoiceField(queryset=TimeSlot.objects.filter(meeting__isnull=True))
-    
-    # fix display for timeslots
-    def __init__(self, *args, **kwargs):
-        super(MeetingAdminForm, self).__init__(*args, **kwargs)
+    # timeslot = forms.ModelChoiceField(queryset=TimeSlot.objects.filter(meeting__isnull=True))
 
-        # if we already have this meeting created (id!=None)
-        # we have to allow the already assigned meeting timeslot to the filtered list
-        if self.instance.id:
-            self.fields['timeslot'] = forms.ModelChoiceField(queryset=TimeSlot.objects.filter(
-                Q(meeting__isnull=True) | Q(meeting__id=self.instance.id)
-        ))
+    # # fix display for timeslots
+    # def __init__(self, *args, **kwargs):
+    #     super(MeetingAdminForm, self).__init__(*args, **kwargs)
+    #     # if we already have this meeting created (id!=None)
+    #     # we have to allow the already assigned meeting timeslot to the filtered list
+    #     if self.instance.id:
+    #         self.fields['timeslot'] = forms.ModelChoiceField(queryset=TimeSlot.objects.filter(
+    #             Q(meeting__isnull=True) | Q(meeting__id=self.instance.id)
+    #         ))
             
     class Meta:
         model = Meeting
