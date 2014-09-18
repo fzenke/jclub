a5d7998159cf004818b217aa9f783f9464480cd3
Alex Seeholzer <seeholzer@gmail.com>
2014-09-18 15:42:17 +0200

    fixed indent to 4 spaces

--- jclub/apps/meetings/models.py
+++ jclub/apps/meetings/models.py
@@ -3,29 +3,30 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Category(models.Model):
-      name = models.CharField(max_length=200)
-      def __unicode__(self):
-          return self.name
+    name = models.CharField(max_length=200)
+    def __unicode__(self):
+        return self.name
 
-      class Meta:
+    class Meta:
         verbose_name_plural = "categories"
 
 class TimeSlot(models.Model):
-      date_time = models.DateTimeField('date and time')
+    date_time = models.DateTimeField('date and time')
       
-      def __unicode__(self):
-          return self.date_time.strftime('%a, %d.%m.%Y @ %H:%M')
+    def __unicode__(self):
+        return self.date_time.strftime('%a, %d.%m.%Y @ %H:%M')
 
-      class Meta:
-          ordering = ["date_time"]      
+    class Meta:
+        ordering = ["date_time"]      
 
 class Meeting(models.Model):
-      presenter = models.ForeignKey(User, unique=False)
-      timeslot  = models.ForeignKey(TimeSlot, unique=True)
-      location  = models.CharField(max_length=100)
-      publication_reference = models.CharField(max_length=200)
-      publication_url       = models.CharField(max_length=200)
-      publication_category  = models.ManyToManyField(Category)
-      def __unicode__(self):
-          return u'%s: %s %s' % (self.timeslot.date_time.strftime('%a, %d.%m.%Y @ %H:%M'), self.presenter.first_name, self.presenter.last_name)
+    presenter = models.ForeignKey(User, unique=False)
+    timeslot  = models.ForeignKey(TimeSlot, unique=True)
+    location  = models.CharField(max_length=100)
+    publication_reference = models.CharField(max_length=200)
+    publication_url       = models.CharField(max_length=200)
+    publication_category  = models.ManyToManyField(Category)
+
+    def __unicode__(self):
+        return u'%s: %s %s' % (self.timeslot.date_time.strftime('%a, %d.%m.%Y @ %H:%M'), self.presenter.first_name, self.presenter.last_name)
 
