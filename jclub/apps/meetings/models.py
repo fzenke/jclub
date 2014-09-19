890f0c7636fa0325cf3bcfd64f0c3e1e188178f5
Alex Seeholzer <seeholzer@gmail.com>
2014-09-19 18:41:48 +0200

    proper timezone display in the admin app

--- jclub/apps/meetings/models.py
+++ jclub/apps/meetings/models.py
@@ -3,6 +3,7 @@ from django.contrib.auth.models import User
 from django.utils import timezone
 import datetime
 from django.conf import settings
+from django.template import defaultfilters
 
 # Create your models here.
 class Category(models.Model):
@@ -18,7 +19,8 @@ class TimeSlot(models.Model):
     date_time = models.DateTimeField('date and time')
       
     def __unicode__(self):
-        return self.date_time.strftime('%a, %d.%m.%Y @ %H:%M')
+        loc_date = timezone.localtime(self.date_time)
+        return defaultfilters.date(loc_date,'M. d. Y @ P')
 
     class Meta:
         ordering = ["date_time"]      
@@ -32,7 +34,8 @@ class Meeting(models.Model):
     publication_category  = models.ManyToManyField(Category)
 
     def __unicode__(self):
-        return u'%s: %s %s' % (self.timeslot.date_time.strftime('%a, %d.%m.%Y @ %H:%M'), self.presenter.first_name, self.presenter.last_name)
+        loc_date = timezone.localtime(self.timeslot.date_time)
+        return u'%s: %s %s' % (defaultfilters.date(loc_date,'M. d. Y @ P'), self.presenter.first_name, self.presenter.last_name)
     
     class Meta:
         ordering = ["-timeslot__date_time"] 
